4aca13cdb396d03529f86b4c6a7b8bfe0dafe1c2
Максимус <s.h.v.ed@yandex.ru>
2023-04-26 21:40:38 +0300

    3. Тесты для определения расстояния между точками. [#159459]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -8,10 +8,12 @@ public class Point {
     public static void main(String[] args) {
         double result = Point.distance(0, 0, 2, 0);
         System.out.println("result (0, 0) to (2, 0) " + result);
-        double result1 = Point.distance(3, 0, 5, 5);
+        double result1 = Point.distance(0, 0, 5, 5);
         System.out.println("result (0, 0) to (5, 5) " + result1);
         double result2 = Point.distance(4, 2, 0, 7);
         System.out.println("result (4, 2) to (0, 7) " + result2);
+        double result3 = Point.distance(5, 6, 6, 5);
+        System.out.println("result (5, 6) to (6, 5) " + result3);
     }
 }
 
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -1,9 +1,10 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
-import org.junit.Test;
 
-public class PointTestTest {
+import org.junit.Test;
+import org.junit.Assert;
+
+public class PointTest {
 
     @Test
     public void when00to20then2() {
@@ -17,9 +18,9 @@ public class PointTestTest {
     }
 
     @Test
-    public void when30to55then5dot38() {
-        double expected = 5.385164807134504;
-        int x1 = 3;
+    public void when00to55then5dot38() {
+        double expected = 7.07;
+        int x1 = 0;
         int y1 = 0;
         int x2 = 5;
         int y2 = 5;
@@ -29,7 +30,7 @@ public class PointTestTest {
 
     @Test
     public void when42to07then6dot40() {
-        double expected = 6.4031242374328485;
+        double expected = 6.40;
         int x1 = 4;
         int y1 = 2;
         int x2 = 0;
@@ -39,12 +40,12 @@ public class PointTestTest {
     }
 
     @Test
-    public void when26to06then2() {
-        double expected = 2;
-        int x1 = 2;
+    public void when56to65then1dot41() {
+        double expected = 1.41;
+        int x1 = 5;
         int y1 = 6;
-        int x2 = 0;
-        int y2 = 6;
+        int x2 = 6;
+        int y2 = 5;
         double out = Point.distance(x1, y1, x2, y2);
         Assert.assertEquals(expected, out, 0.01);
     }
