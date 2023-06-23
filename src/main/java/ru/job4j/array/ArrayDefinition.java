d9fe62f4d01bdf3fc0e088d49bb870bb478c496a
Максимус <s.h.v.ed@yandex.ru>
2023-06-23 22:23:26 +0300

    6.0.3. Заполнение массива. [#226300 # [#226300]]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,11 +2,14 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        String[] surnames = new String[100500];
-        float[] prices = new float[40];
-        System.out.println("the size of the array is: " + ages.length);
-        System.out.println("the size of the array is: " + surnames.length);
-        System.out.println("the size of the array is: " + prices.length);
+        String[] nums = new String[4];
+        nums[0] = "Max";
+        nums[1] = "Ira";
+        nums[2] = "Oleg";
+        nums[3] = "Platon";
+        System.out.println(nums[0]);
+        System.out.println(nums[1]);
+        System.out.println(nums[2]);
+        System.out.println(nums[3]);
     }
 }
